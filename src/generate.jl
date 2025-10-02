feddbf66a6bb8e21b88d0f485e5a1fce38aeb220
Josh Day <emailjoshday@gmail.com>
2025-10-02 15:29:43 -0400

    update script

--- src/generate.jl
+++ src/generate.jl
@@ -14,44 +14,102 @@ cp(joinpath(@__DIR__, "..", "CODE", "model_single_3D"), joinpath(DIR, "model_sin
 # Number of runs to perform; default is 10 if not provided as command line argument
 N = length(ARGS) == 1 ? parse(Int, ARGS[1]) : 10
 
-#-----------------------------------------------------------------------------# common_args
-common_args = (
-    Model = "minimal",
-    ISO = 1,
-    Jup_scale = 2,
-    tau_ss_type = "medium_fast",
-    BCL = 350,
-    Reference = "SR",
-    Spatial_output_interval_data = 0,
-    Spatial_output_interval_vtk = 0,
+#-----------------------------------------------------------------------------# notes
+# Low adrenergic / Low SR: ISO 0, BCL 1000, Jup_scale 1.0, tau_ss_type medium_low
+# High adrenergic / High SR: ISO 1, BCL 350, Jup_scale 2.0, tau_ss_type medium_fast,
+
+
+#-----------------------------------------------------------------------------# low
+low = (
+    common_args = (
+        Model = "minimal",
+        ISO = 0,
+        Jup_scale = 1,
+        tau_ss_type = "medium_low",
+        BCL = 1000,
+        Reference = "sr_low",
+        Spatial_output_interval_data = 0,
+        Spatial_output_interval_vtk = 0,
+    ),
+    prepace_args = (
+        Beats = 40,
+        Sim_cell_size = "testing",
+        Write_state = "ave",
+        Results_Reference = "prepace",
+        Total_time = 40 * 1000,
+    ),
+    prepace_full_args = (
+        Beats = 4,
+        Sim_cell_size = "full",
+        Read_state = "ave",
+        Write_state = "On",
+        Results_Reference = "prepace_full",
+        Total_time = 4 * 1000,
+    ),
+    run_args = (
+        Beats = 1,
+        Sim_cell_size = "full",
+        Read_state = "On",
+        total_time = 1100
+    )
 )
 
-#-----------------------------------------------------------------------------# prepace
-prepace_args = (
-    Beats = 50,
-    Sim_cell_size = "testing",
-    Write_state = "ave",
-    Results_Reference = "prepace",
-    Total_time = 50 * 350,
+#-----------------------------------------------------------------------------# high
+high = (
+    common_args = (
+        Model = "minimal",
+        ISO = 1,
+        Jup_scale = 1,
+        tau_ss_type = "medium_fast",
+        BCL = 350,
+        Reference = "sr_high",
+        Spatial_output_interval_data = 0,
+        Spatial_output_interval_vtk = 0,
+    ),
+    prepace_args = (
+        Beats = 40,
+        Sim_cell_size = "testing",
+        Write_state = "ave",
+        Results_Reference = "prepace",
+        Total_time = 40 * 350,
+    ),
+    prepace_full_args = (
+        Beats = 4,
+        Sim_cell_size = "full",
+        Read_state = "ave",
+        Write_state = "On",
+        Results_Reference = "prepace_full",
+        Total_time = 4 * 350,
+    ),
+    run_args = (
+        Beats = 1,
+        Sim_cell_size = "full",
+        Read_state = "On",
+        total_time = 1100
+    )
 )
 
-#-----------------------------------------------------------------------------# prepace_full
-prepace_full_args = (
-    Beats = 4,
-    Sim_cell_size = "full",
-    Read_state = "ave",
-    Write_state = "On",
-    Results_Reference = "prepace_full",
-    Total_time = 4 * 350,
-)
+#-----------------------------------------------------------------------------# init
+# low
+run_model(merge(low.common_args, low.prepace_args))
+run_model(merge(low.common_args, low.prepace_full_args))
+# high
+run_model(merge(high.common_args, high.prepace_args))
+run_model(merge(high.common_args, high.prepace_full_args))
 
-#-----------------------------------------------------------------------------# run_args
-run_args = (
-    Beats = 1,
-    Sim_cell_size = "full",
-    Read_state = "On",
-    total_time = 1000
-)
+#-----------------------------------------------------------------------------# runs
+for _ in 1:N
+    # Make sure we don't overwrite existing runs
+    n = length(filter(x -> startswith(x, r"Results_run"), readdir(joinpath(@__DIR__, "..", "data", "Outputs_3Dcell_sr_low")))) + 1
+    args = merge(low.common_args, low.run_args, (; Results_Reference = "run_$(lpad(n, 3, '0'))"))
+    @info "Running low SR Run $n" args
+    run_model(args)
+
+    n = length(filter(x -> startswith(x, r"Results_run"), readdir(joinpath(@__DIR__, "..", "data", "Outputs_3Dcell_sr_high")))) + 1
+    args = merge(high.common_args, high.run_args, (; Results_Reference = "run_$(lpad(n, 3, '0'))"))
+    @info "Running high SR Run $n" args
+    run_model(args)
+end
 
 
 #-----------------------------------------------------------------------------# functions
@@ -64,26 +122,12 @@ function make_cmd(args)
 end
 
 function run_model(args)
-    cd(joinpath(@__DIR__, "..", "data")) do
-        @time run(make_cmd(args))
+    results_dir = joinpath(DIR, "Outputs_3Dcell_$(args.Reference)")
+    if isdir(results_dir)
+        @info "`$(args.Reference)` results directory already exists, skipping..."
+    else
+        cd(joinpath(@__DIR__, "..", "data")) do
+            @time run(make_cmd(args))
+        end
     end
-end
-
-#-----------------------------------------------------------------------------# runs
-if isdir(joinpath(DIR, "Outputs_3Dcell_SR", "Results_prepace"))
-    @info "`prepace` already done, skipping..."
-else
-    run_model(prepace_args)  # ~2.5 minutes for 50 beats at 250ms BCL
-end
-
-if isdir(joinpath(DIR, "Outputs_3Dcell_SR", "Results_prepace_full"))
-    @info "`prepace_full` already done, skipping..."
-else
-    run_model(prepace_full_args)  # ~7.5 minutes for 4 beats at 350ms BCL
-end
-
-for i in 1:N
-    n = length(filter(x -> startswith(x, r"Results_run"), readdir(joinpath(@__DIR__, "..", "data", "Outputs_3Dcell_SR")))) + 1
-    @info "Run: $n"
-    run_model(merge(run_args, (; Results_Reference = "run_$(lpad(n, 3, '0'))")))
 end
